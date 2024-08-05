fd8f24742190699d2765fd1d96aa5f251a8230f3
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2024-08-05 20:41:30 +0700

    Pertemuan 4 Operator Pada Java

---
+++ pertemuan4soal1/Pertemuan4soal1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4soal1;
+
+/**
+ *
+ * @author USER
+ */
+public class Pertemuan4soal1 {
+
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 3;
+
+        System.out.println("Hasil Pertambahan A + B = "
+                + (A + B));
+        System.out.println("Hasil Pengurangan A - B = "
+                + (A - B));
+        System.out.println("Hasil Perkalian A * B = "
+                + (A * B));
+        System.out.println("Hasil Pembagian A / B: "
+                + (A / B));
+    }
+}
