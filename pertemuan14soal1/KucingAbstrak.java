397eaec6b83788756e7b075da9c26cc71aa8bab9
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2024-08-07 23:11:45 +0700

    Pertemuan 14 Abstract Class Interface

---
+++ pertemuan14soal1/KucingAbstrak.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pertemuan14soal1;
+
+/**
+ *
+ * @author USER
+ */
+public class KucingAbstrak extends AbstrakHewan {
+    @Override
+    public void suara() {
+        System.out.println(" Suara kucing mengeong");
+    }
+}
