03fedf4c24d944c298f326f560b37432c512623f
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2024-08-08 20:32:41 +0700

    Pertemuan 14 Abstract Class Interface

---
+++ pertemuan14soal1/src/main/java/com/mycompany/pertemuan14soal1/Pertemuan14soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan14soal1;
+
+/**
+ *
+ * @author USER
+ */
+
+public class Pertemuan14soal1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AbstrakHewan objek = new KucingAbstrak();
+        objek.suara();
+        objek.suara2();
+    }
+}
